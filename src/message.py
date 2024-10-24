235938af83b103701462b81bb313d6cac64e0db4
Benedikt Schwering <mail@bschwer.ing>
2024-10-24 17:21:30 +0200

    feat(message): add new bgp update message definition

--- src/message.py
+++ src/message.py
@@ -1,24 +1,50 @@
 from pydantic import BaseModel
-from typing import Optional
+from datetime import datetime
+from enum import Enum
 
-class _Address(BaseModel):
-    local: str
-    peer: str
+class OriginType(Enum):
+    IGP = 1
+    EGP = 2
+    INCOMPLETE = 3
 
-class _Asn(BaseModel):
-    local: int
-    peer: int
+class AsPathType(Enum):
+    AS_SET = 1
+    AS_SEQUENCE = 2
+    AS_CONFED_SET = 3
+    AS_CONFED_SEQUENCE = 4
 
-class _Update(BaseModel):
-    attribute: Optional[dict] = None
-    announce: Optional[dict] = None
-    withdraw: Optional[dict] = None
+class AsPath(BaseModel):
+    type: AsPathType
+    value: list[int]
 
-class _Message(BaseModel):
-    update: Optional[_Update] = None
+class Aggregator(BaseModel):
+    router_id: str
+    router_as: int
 
-class Message(BaseModel):
-    address: _Address
-    asn: _Asn
-    direction: str
-    message: _Message
+class Network(BaseModel):
+    prefix: str
+    mask: int
+
+class PathAttributes(BaseModel):
+    origin: OriginType = None
+    as_path: AsPath = None
+    next_hop: list[str] = []
+    multi_exit_disc: int = 0
+    local_pref: int = 100
+    atomic_aggregate: bool = False
+    aggregator: Aggregator = None
+    community: list[list[int]] = [] # including large community
+    orginator_id: str = None
+    cluster_list: list[str] = []
+    mp_reach_nlri: list[Network] = []
+    mp_unreach_nlri: list[Network] = []
+    extended_community: list[int] = []
+
+class BGPUpdateMessage(BaseModel):
+    timestamp: datetime
+    peer_ip: str
+    local_ip: str
+    peer_as: int
+    local_as: int
+    withdrawn_routes: list[Network]
+    path_attributes: list[PathAttributes]
