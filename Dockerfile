4d245e46709e1a37b71a6637c22b5b7dac96bd4d
Benedikt Schwering <mail@bschwer.ing>
2024-11-14 17:09:55 +0100

    feat(zettabgp): add ui to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,24 @@
+FROM node:lts-alpine AS ui
+
+WORKDIR /app
+
+COPY ui/ .
+
+RUN npm install
+RUN npm run build
+
+
 FROM python:3.10
 
 WORKDIR /app/zettabgp
 
 COPY requirements.txt .
 RUN pip3 install -r requirements.txt
+
+COPY --from=ui /app/dist/ui/browser/ /app/zettabgp/src/ui/
+ENV ZETTABGP_WEBAPP_UI_PATH=/app/zettabgp/src/ui
+ENV ZETTABGP_WEBAPP_APP=src.webapp:app
+ENV ZETTABGP_WEBAPP_MRT_LIBRARY_PATH=mrt
 
 COPY setup.py .
 COPY src/ src/
--- src/webapp.py
+++ src/webapp.py
@@ -1,11 +1,16 @@
+from src.controllers.mrt_library import mrt_library_router
 from src.controllers.version import version_router
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi import FastAPI
-import uvicorn
+import uvicorn, os
 
 app = FastAPI()
 
+app.include_router(
+    router=mrt_library_router,
+    prefix='/api/mrt-library'
+)
 app.include_router(
     router=version_router,
     prefix='/api/version'
@@ -14,22 +19,22 @@ app.include_router(
 app.mount(
     path='/ui',
     app=StaticFiles(
-        directory='src/ui'
+        directory=os.getenv('ZETTABGP_WEBAPP_UI_PATH', 'src/ui'),
     ),
     name="ui",
 )
 
 @app.get('/')
 def serve_angular_root():
-    return FileResponse('src/ui/index.html')
+    return FileResponse(os.getenv('ZETTABGP_WEBAPP_UI_PATH', 'src/ui') + '/index.html')
 
 @app.exception_handler(404)
 def serve_angular_root(_, __):
-    return FileResponse('src/ui/index.html')
+    return FileResponse(os.getenv('ZETTABGP_WEBAPP_UI_PATH', 'src/ui') + '/index.html')
 
 def start_webapp(reload: bool):
     uvicorn.run(
-        app='src.webapp:app',
+        app=os.getenv('ZETTABGP_WEBAPP_APP', 'src.webapp:app'),
         host='0.0.0.0',
         port=8000,
         reload=reload,
