a61390cf43dedd3f9f46d85ee6c2acb1eca6ecf3
Benedikt Schwering <mail@bschwer.ing>
2024-11-30 10:41:50 +0100

    chore(version): update zettabgp version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='ZettaBGP',
-    version='0.2.1',
+    version='0.2.2',
     description='',
     url='https://git.univ.leitwert.net/imprj/01-bgp-testbed/zettabgp',
     author='Benedikt Schwering & Sebastian Forstner',
