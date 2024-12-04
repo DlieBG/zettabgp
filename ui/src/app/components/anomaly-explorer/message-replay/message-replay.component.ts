60d2753563cf670b74dcbe65c1e957a4deafcf00
Benedikt Schwering <mail@bschwer.ing>
2024-12-04 09:05:49 +0100

    fix(ui): remove double negotiation

--- ui/src/app/components/anomaly-explorer/message-replay/message-replay.component.ts
+++ ui/src/app/components/anomaly-explorer/message-replay/message-replay.component.ts
@@ -9,17 +9,15 @@ import { MessageReplayService } from '../../../services/message-replay/message-r
   styleUrl: './message-replay.component.scss'
 })
 export class MessageReplayComponent {
-  message_replay_request: MessageReplayRequest = {
-    no_rabbitmq_direct: false,
-    rabbitmq_grouped: null,
-    no_mongodb_log: false,
-    no_mongodb_state: false,
-    no_mongodb_statistics: false,
-    clear_mongodb: true,
-    playback_speed: null,
-    start_time: '2024-10-01T00:00',
-    end_time: '2024-12-01T00:00',
-  };
+  rabbitmq_direct = true;
+  rabbitmq_grouped = null;
+  mongodb_log = false;
+  mongodb_state = false;
+  mongodb_statistics = false;
+  clear_mongodb = true;
+  playback_speed = null;
+  start_time = '2024-10-01T00:00';
+  end_time = '2024-12-01T00:00';
 
   messageReplayResponse$!: Observable<MessageReplayResponse>;
 
@@ -28,6 +26,16 @@ export class MessageReplayComponent {
   ) { }
 
   startMessageReplay() {
-    this.messageReplayResponse$ = this.messageReplayService.startMessageReplay(this.message_replay_request);
+    this.messageReplayResponse$ = this.messageReplayService.startMessageReplay({
+      no_rabbitmq_direct: !this.rabbitmq_direct,
+      rabbitmq_grouped: this.rabbitmq_grouped,
+      no_mongodb_log: !this.mongodb_log,
+      no_mongodb_state: !this.mongodb_state,
+      no_mongodb_statistics: !this.mongodb_statistics,
+      clear_mongodb: this.clear_mongodb,
+      playback_speed: this.playback_speed,
+      start_time: this.start_time,
+      end_time: this.end_time,
+    });
   }
 }
